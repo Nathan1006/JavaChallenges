815c2eff99c83717a6447c8281176c0fda173f40
Nathan1006 <nrpetit@outlook.com>
2021-09-28 18:26:28 -0400

    Day 0 of Hackernet

--- HackerRankChallenges/Day0.java
+++ HackerRankChallenges/Day0.java
@@ -4,8 +4,10 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String inputString = sc.nextLine();
         System.out.println("Hello, World.");
-        System.out.println("Welcome to 30 Days of Code!");
+        System.out.println(inputString);
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
     }
 }
